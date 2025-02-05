5e6fcd0ade02e15404ccae7ce6cf2f4299bccbc9
Li_Jiongyi <133729700+LIJiongyi@users.noreply.github.com>
2025-02-05 15:59:04 +0800

    ini setup

---
+++ MacRightMouse/MacRightMouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacRightMouseApp.swift
+//  MacRightMouse
+//
+//  Created by 李炯一 on 5/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct MacRightMouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacRightMouseTests/MacRightMouseTests.swift
@@ -0,0 +1,16 @@
+//
+//  MacRightMouseTests.swift
+//  MacRightMouseTests
+//
+//  Created by 李炯一 on 5/2/2025.
+//
+
+import Testing
+
+struct MacRightMouseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
